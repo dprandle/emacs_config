9894411e65d236e540b69929f77ee37627cc85e5
dprandle <dprandle@alaska.edu>
2023-01-23 11:49:14 -0900

    Updating init el

--- init.el
+++ init.el
@@ -12,12 +12,16 @@
 (require 'use-package)
 (use-package lsp-ui)
 
+(require 'iedit)
+
 ;; sample `helm' configuration use https://github.com/emacs-helm/helm/ for details
 (helm-mode)
 (require 'helm-xref)
 (define-key global-map [remap find-file] #'helm-find-files)
 (define-key global-map [remap execute-extended-command] #'helm-M-x)
 (define-key global-map [remap switch-to-buffer] #'helm-mini)
+(define-key global-map [remap other-window] #'ace-select-window)
+(define-key global-map [remap delete-other-windows] #'ace-delete-other-windows)
 (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to do persistent action
 (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB works in terminal
 (define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
@@ -28,6 +32,9 @@
 
 ;; Turn on superword mode by defaul
 (global-subword-mode 1)
+
+;; highlight matching paren
+(show-paren-mode 1)
 
 (setq gc-cons-threshold (* 100 1024 1024)
       read-process-output-max (* 1024 1024)
@@ -83,8 +90,7 @@
 
 (defun open-with-designer()
   (interactive)
-  (shell-command (format "nohup designer %s </dev/null &>/dev/null &" (buffer-file-name)))
-  )
+  (shell-command (format "nohup designer %s </dev/null &>/dev/null &" (buffer-file-name))))
 
 (defun open-scratch-buffer()
   "Create or get scratch buffer"
@@ -153,21 +159,15 @@
 (add-hook 'shell-mode-hook 'set-no-process-query-on-exit)
 
 ;; define custom keys..
-(global-set-key (kbd "M-m") 'helm-imenu)
 (global-set-key (kbd "M-s") 'isearch-forward-symbol-at-point)
-(define-key isearch-mode-map (kbd "M-s") 'isearch-repeat-forward)
-(global-set-key (kbd "M-=") 'other-window)
 (global-set-key (kbd "M-k") 'kill-this-buffer)
-(global-set-key (kbd "M-K") 'delete-window)
 (global-set-key (kbd "M-:") 'xref-find-references)
 (global-set-key (kbd "M-j") 'xref-find-definitions)
 (global-set-key (kbd "M-J") 'find-definition-other-window)
 (global-set-key (kbd "C-|") 'balance-windows)
 (global-set-key (kbd "C-\\") 'split-window-right)
 (global-set-key (kbd "M-|") 'reset-splits)
-(global-set-key (kbd "M-q") 'pop-global-mark)
-(global-set-key (kbd "C-t") 'shell)
-(global-set-key (kbd "M-p") 'backward-paragraph)
+
 (global-set-key (kbd "M-p") 'backward-paragraph)
 (global-set-key (kbd "M-n") 'forward-paragraph)
 (global-set-key (kbd "C->") 'next-error)
@@ -177,59 +177,67 @@
 (global-set-key (kbd "M-M") 'add-line-above)
 (global-set-key (kbd "C-S-m") 'add-line-below)
 
+;; Unset arrow keys and undo key so that I stop using them by accident
+(global-unset-key (kbd "<left>"))
+(global-unset-key (kbd "<right>"))
+(global-unset-key (kbd "<up>"))
+(global-unset-key (kbd "<down>"))
+(global-unset-key (kbd "C-z"))
+
 ;; map M-h [mark-paragraph] to M-backslash
 (global-set-key (kbd "M-\\") 'mark-paragraph)
 
+;; Rebind/add bindings to a couple ctrl-x items - ones that seem like they should be there from the start
 (define-key ctl-x-map (kbd "C-S-f") 'find-file-other-window)
-(define-key ctl-x-map (kbd "C-m") 'open-scratch-buffer)
-(define-key ctl-x-map (kbd "C-n") 'open-shared-notes)
-(define-key ctl-x-map (kbd "C-,") 'open-settings)
-(define-key ctl-x-map (kbd "C-.") 'projectile-edit-dir-locals)
-(define-key ctl-x-map (kbd "C-p") 'projectile-switch-project)
-(define-key ctl-x-map (kbd "C-g") 'magit-status)
+(define-key ctl-x-map (kbd "C-b") 'helm-mini)
+(define-key ctl-x-map (kbd "b") 'list-buffers)
+(define-key ctl-x-map (kbd "f") 'projectile-find-file)
+(define-key ctl-x-map (kbd "F") 'projectile-find-file-other-window)
 
-(define-key goto-map (kbd "s") 'magit-stage-modified)
-(define-key goto-map (kbd "S") 'magit-unstage-all)
-(define-key goto-map (kbd "c") 'magit-commit)
-(define-key goto-map (kbd "p") 'magit-push)
-(define-key goto-map (kbd "f") 'magit-pull)
-(define-key goto-map (kbd "S-f") 'magit-fetch-all-prune)
-(define-key goto-map (kbd "b") 'magit-branch)
+(global-set-key (kbd "C-c C-f") 'helm-imenu)
+(global-set-key (kbd "C-c C-m") 'open-scratch-buffer)
+(global-set-key (kbd "C-c C-n") 'open-shared-notes)
+(global-set-key (kbd "C-c C-,") 'open-settings)
+(global-set-key (kbd "C-c C-.") 'projectile-edit-dir-locals)
+(global-set-key (kbd "C-c C-p") 'projectile-switch-project)
+(global-set-key (kbd "C-c C-t") 'shell)
+(global-set-key (kbd "C-c g") 'magit-file-dispatch)
 
+;; Multi cursor select
 (global-set-key (kbd "M-S") 'mc/mark-next-like-this)
 (global-set-key (kbd "M-R") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-;") 'mc/mark-all-like-this)
 (global-set-key (kbd "C-<next>") 'mc/mark-next-lines)
 (global-set-key (kbd "C-<prior>") 'mc/mark-previous-lines)
 
 (global-set-key (kbd "M-+") 'windmove-swap-states-right)
 (global-set-key (kbd "M-_") 'windmove-swap-states-left)
 
-(global-set-key (kbd "<f4>") 'open-with-designer)
 
 (defun proj-comp()
   (interactive)
-  (other-window 1)
-  (call-interactively #'projectile-compile-project))
+  (call-interactively #'projectile-compile-project)
+  (switch-to-prev-buffer)
+  (switch-to-buffer-other-window "*compilation*"))
 
 (defun proj-conf()
   (interactive)
-  (other-window 1)
-  (call-interactively #'projectile-configure-project))
+  (call-interactively #'projectile-configure-project)
+  (switch-to-prev-buffer)
+  (switch-to-buffer-other-window "*compilation*"))
 
 (defun proj-run()
   (interactive)
-  (other-window 1)
-  (call-interactively #'projectile-run-project))
+  (call-interactively #'projectile-run-project)
+  (switch-to-prev-buffer)
+  (switch-to-buffer-other-window "*compilation*"))
 
+(global-set-key (kbd "<f4>") 'open-with-designer)
 (global-set-key (kbd "<f5>") 'proj-comp)
 (global-set-key (kbd "<f6>") 'proj-conf)
 (global-set-key (kbd "<f7>") 'proj-run)
 (global-set-key (kbd "M-<f11>") 'toggle-frame-fullscreen)
 
-(define-key lsp-ui-mode-map (kbd "M-;") 'lsp-ui-peek-find-references)
-(global-set-key (kbd "s-f") 'projectile-find-file)
-
+(define-key lsp-mode-map (kbd "M-;") 'lsp-ui-peek-find-references)
 (define-key lsp-mode-map [remap xref-find-definitions] #'lsp-find-definition)
 (define-key lsp-mode-map [remap xref-find-references] #'lsp-find-references)
 (define-key lsp-mode-map (kbd "C-:") 'lsp-rename)
@@ -252,20 +260,12 @@
 (define-key lsp-mode-map (kbd "<f12>") 'dap-step-out)
 (define-key lsp-mode-map (kbd "M-<f10>") 'end-debug)
 
-
 (setq projectile-project-search-path '("~/projects"))
 (projectile-discover-projects-in-search-path)
 (setq projectile-switch-project-action #'projectile-dired)
 
 (require 'cc-mode)
 (electric-pair-mode)
-
-(defun my-c++-mode-hook ()
-  (c-set-style "my-style")        ; use my-style defined above
-  (auto-fill-mode)         
-  (c-toggle-auto-hungry-state -1))
-
-(add-hook 'c++-mode-hook 'my-c++-mode-hook)
 
 ;; Move text and selected regions up and down
 (defun move-text-internal (arg)
@@ -345,7 +345,9 @@
                                   :src-dir "src/"
 				  )
 
-(set-face-attribute 'default nil :family "Ubuntu Mono" :height 132)
+(if (eq system-type 'gnu/linux)
+    (set-face-attribute 'default nil :family "Ubuntu Mono" :height 132))
+
 (if (eq system-type 'darwin)
     (set-face-attribute 'default nil :family "Menlo" :height 112))
 
@@ -363,14 +365,22 @@
     (save-excursion (goto-char (point-max)) (insert msg))
     (message "Compilation %s: %s" (string-trim-right why) msg)))
 
-
 (defun add-func-calls-to-name-face()
   (font-lock-add-keywords
    nil
    '(("\\<\\(\\sw+\\)?(" 1 'font-lock-function-name-face))))
 
-(add-hook 'c-mode-hook 'add-func-calls-to-name-face)
-(add-hook 'c++-mode-hook 'add-func-calls-to-name-face)
+
+(defun my-c++-mode-hook ()
+  (c-set-style "my-style")        ; use my-style defined above
+  (auto-fill-mode)         
+  (c-toggle-auto-hungry-state -1)
+  ;; Remove the C-c C-p binding in c++-mode-map
+  (define-key c++-mode-map (kbd "C-c C-p") nil)
+  (add-func-calls-to-name-face))
+
+(add-hook 'c-mode-hook 'my-c++-mode-hook)
+(add-hook 'c++-mode-hook 'my-c++-mode-hook)
 (add-hook 'c++-mode-hook #'modern-c++-font-lock-mode)
 
 (c-add-style "MyStyle"
@@ -461,7 +471,6 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(cua-mode t nil (cua-base))
  '(custom-safe-themes
    '("78e6be576f4a526d212d5f9a8798e5706990216e9be10174e3f3b015b8662e27" "e09401ab2c457e2e4d8b800e1c546dbc8339dc33b2877836ba5d9b6294ae6e55" "0ed28b0694dd2c7a2407598e63650a8562b9e833a1a136ee74790a74d3776d3b" default))
  '(dap-mode t nil (dap-mode))
@@ -472,14 +481,14 @@
  '(ns-command-modifier 'control)
  '(ns-control-modifier 'meta)
  '(package-selected-packages
-   '(modern-cpp-font-lock clang-format lsp-mode lsp-ui yasnippet lsp-treemacs helm-lsp projectile hydra flycheck company avy which-key helm-xref dap-mode multiple-cursors))
+   '(iedit ace-window treemacs-projectile modern-cpp-font-lock clang-format lsp-mode lsp-ui yasnippet lsp-treemacs helm-lsp projectile hydra flycheck company avy which-key helm-xref dap-mode multiple-cursors))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#1e1e1e" :foreground "wheat1" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 142 :width normal :foundry "nil" :family "Courier New"))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#1e1e1e" :foreground "wheat1" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 132 :width normal :foundry "nil" :family "Ubuntu Mono"))))
  '(button ((t (:inherit link :foreground "#ce9178"))))
  '(error ((t (:foreground "light pink" :weight bold))))
  '(font-lock-constant-face ((t (:foreground "#4ec9b0"))))
@@ -494,4 +503,7 @@
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
- 
+
+;; These need to go last because the above changes default font - this will change for mac and linux
+(if (eq system-type 'darwin)
+    (set-face-attribute 'default nil :family "Menlo" :height 112))
